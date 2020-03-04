2191de58fd22629b54fe65b19e28dad4743ea586
Julio Claros <60437584+JulioClaros321@users.noreply.github.com>
2020-03-04 10:15:34 -0500

    lol

---
+++ lab_4/scripts/lab_4.js
@@ -0,0 +1,37 @@
+const myTitle = document.querySelector("title");
+const myHeading = document.querySelector("p");
+myHeading.textContent = "Hello world!";
+
+myTitle.textContent = "Julio Claros' Lab_4";
+var br = document.createElement("br");
+
+var name = "Julio Claros";
+var first_name = name.substring(0, 5)
+var last_name = name.substring(5, 11)
+var count = name.length - 1
+var counter = 3
+
+var total = first_name.length + last_name.length
+
+const myHeader = document.querySelector(".header");
+myHeader.textContent = "Julio Claros' Lab 4 " + total;
+
+
+var paragraph = document.createElement("p");
+var text = document.createTextNode("My name has " +count +" characters");
+paragraph.appendChild(text);
+paragraph.classList.add("para1");
+
+var sec_paragraph = document.createElement("p");
+var sec_text = document.createTextNode("The third character in my name is " +name[2].toUpperCase());
+var third_text = document.createTextNode("lio")
+sec_paragraph.appendChild(sec_text);
+sec_paragraph.classList.add("para2");
+sec_paragraph.appendChild(br)
+sec_paragraph.appendChild(third_text)
+
+var content = document.querySelector(".content"); 
+
+content.appendChild(paragraph);
+content.appendChild(sec_paragraph);
+
--- lab_4/scripts/main.js
+++ lab_4/scripts/main.js
@@ -1,2 +1,2 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = "Hello World";
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
