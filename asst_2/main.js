4d118b846fac745a59da9a09afb0290db23d3475
Julio Claros <60437584+JulioClaros321@users.noreply.github.com>
2020-04-01 10:13:03 -0400

    Create main.js

---
+++ asst_2/main.js
@@ -0,0 +1,15 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+var i = 1
+
+
+/* Looping through images */
+    for (i; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(e) {
+  
