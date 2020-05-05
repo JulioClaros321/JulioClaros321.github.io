55cdd4e232d21c44b737cb2e7139205b1da3b276
Julio Claros <60437584+JulioClaros321@users.noreply.github.com>
2020-05-05 18:35:37 -0400

    lol

--- lab_9/server.js
+++ lab_9/server.js
@@ -22,20 +22,63 @@ app.use(express.static('public'));
 
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = ''; // Enter the URL for the data you would like to retrieve here
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; // Enter the URL for the data you would like to retrieve here
 
   // Your Fetch API call starts here
   // Note that at no point do you "return" anything from this function -
   // it instead handles returning data to your front end at line 34.
-    fetch(baseURL)
-      .then((r) => r.json())
-      .then((data) => {
-        console.log(data);
-        res.send({ data: data }); // here's where we return data to the front end
+
+  fetch(' https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json') // replace this with the food safety data set
+  .then((r) => r.json())
+  .then((data) => { // this is an explicit return. If I want my information to go further, I'll need to use the "return" keyword before the brackets close
+      return data
+
+    })
+  .then((data) => {
+    let newDATA = data.filter((r) => r.geocoded_column_1)
+    const refined = newDATA.map((t) => ({
+          category: t.category,
+          name: t.name,
+          location: t.geocoded_column_1.coordinates,
+        }));
+        return refined
       })
-      .catch((err) => {
-        console.log(err);
-        res.redirect('/error');
+
+  .then((data) => {
+    return data.reduce((c, current) => {
+      if (!c[current.category]) {
+
+        c[current.category] = [];
+      }
+      c[current.category].push(current);
+      return c;
+    }, {});
+  })
+  
+  .then((data) => {
+    console.log("new data", data);
+    const reformattedData = Object.entries(data).map((m, i) => {
+      console.log(m)
+      return {
+        y: m[1].length,
+        label: m[0],
+    };
+  });
+  return reformattedData;
+  })
+
+
+  .then((data) => {
+        
+
+    console.log(data);
+    res.send({ data: data }); // here's where we return data to the front end
+
+  })
+      
+  .catch((err) => {
+    console.log(err);
+    res.redirect('/error');
       });
 }
 
