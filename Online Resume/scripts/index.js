7926219ad45c64161b93639d635b2cb164d25128
Julio Claros <60437584+JulioClaros321@users.noreply.github.com>
2022-02-14 09:57:43 -0500

    Finished Transitions, fixed Links that were causing problems

--- Online Resume/scripts/index.js
+++ Online Resume/scripts/index.js
@@ -1,1 +1,31 @@
-console.log("hello")
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready)
+}
+else {
+    ready()
+}
+
+function ready() {
+
+    function myFunction(tags, number) {
+        var div = tags[number]
+        div.scrollIntoView({behavior: "smooth"})
+    }
+
+
+    var tags = document.getElementsByTagName("h1")
+
+    $(".data_science").click(function(){
+        myFunction(tags, 0);
+    });
+
+    $(".javascript").click(function(){
+        myFunction(tags, 1);
+    });
+
+    $(".unity").click(function(){
+        myFunction(tags, 2);
+    });
+
+
+}
